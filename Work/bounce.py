eef2240ef44b2ac219e9554fea8d383a0f368132
dannykozar <73081094+dannykozar@users.noreply.github.com>
2020-12-06 13:29:28 -0500

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+peak = 100 # initial drop height [m]
+bounce_f = 3/5 # bounce height as proportion of previous bounce [unitless]
+
+i = 1
+while i <= 10:
+    peak = bounce_f * peak
+    print(i, peak)
+    i = i + 1
